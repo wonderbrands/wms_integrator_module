2e5dc39fcf71b8826f2596ce5a28adcfe8e3dc50
Demian Avila <demian@wonderbrands.co>
2024-10-30 14:03:52 -0600

    feat: add security group to permissions

--- wms_integrator_module/__manifest__.py
+++ wms_integrator_module/__manifest__.py
@@ -23,9 +23,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/carriers_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [

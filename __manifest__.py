4677b1f83ecd9400c47fdc0ea9712de6c796e8cd
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2025-03-25 01:23:51 +0100

    fix: added gitignore, moved to v18

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0',
+    'version': '18.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale'],
@@ -24,12 +24,7 @@
     # always loaded
     'data': [
         'views/views.xml',
-        'views/templates.xml',
         'views/carriers_view.xml',
         'security/ir.model.access.csv',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
